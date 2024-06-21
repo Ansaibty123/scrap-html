eb80154cd4377a63d8707c798bbedb04bcabd871
Ansaigra Basumatary <164206927+Ansaibty123@users.noreply.github.com>
2024-06-22 00:48:28 +0530

    qsn $ ans updt

--- scrap.js
+++ scrap.js
@@ -1,34 +1,48 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const fs = require('fs');
-const { Parser } = require('json2csv');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const { Parser } = require("json2csv");
 
-const url = 'https://interviewmania.com/gk-general-knowledge/art-and-culture/1/1#';
+const url =
+  "https://interviewmania.com/gk-general-knowledge/art-and-culture/1/1#";
 
-axios.get(url).then(response => {
+axios
+  .get(url)
+  .then((response) => {
     const html = response.data;
     const $ = cheerio.load(html);
     const questions = [];
     // console.log(html);
 
-$('ol[start] li div').each((index,element)=>{
-    const question = $(element).text().trim();
-    console.log(question)
-    const options = [];
-    const id = $('input[form-check-input]').attr('id')
-     console.log(id)
-    // $(element).find('span[style="color:#888;font-size:16px;"]').each((i, el) => {
-    //     options.push($(el).text().trim());
-    //   });
-    //   console.log(options)
+    $("ol[start] li div").each((index, element) => {
+      const question = $(element).text();
+      questions.push({ id: index + 1, question, options: [] ,answers:[]})
+      
+    });
     
+    $(".list-group-item").each((i, el) => {
+        const options = [];
+        $(el).find('.row').each((I,El) => {
+          const option = $(El).find('span').text().trim();
+          options.push(option);
+        });
+       
+        if (questions[i]) {
+          questions[i].options = options;
+        }
+      });
+      $(".collapse").each((i, el) => {
+        const answer = $(el).find("span").text();
+        if (questions[i]) {
+          questions[i].answers = answer;
+        }
+      });
+      console.log(questions)
 
-    
-})
-// const parser = new Parser();
-    // const csv = parser.parse(questions);
-    // fs.writeFileSync('data.csv', csv, 'utf-8');
-
-}).catch(error => {
-    console.log(error)
-})
+    const parser = new Parser();
+    const csv = parser.parse(questions);
+    fs.writeFileSync('data.csv', csv, 'utf-8');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
