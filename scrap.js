b244e5601899554fbfd48b11868dc53d6c37eabd
Ansaigra Basumatary <164206927+Ansaibty123@users.noreply.github.com>
2024-06-28 21:54:26 +0530

    pagination

--- scrap.js
+++ scrap.js
@@ -3,47 +3,68 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 const { Parser } = require("json2csv");
 
-const url =
-  "https://interviewmania.com/gk-general-knowledge/art-and-culture/1/1#";
+const baseurl =
+  "https://interviewmania.com/gk-general-knowledge/international-events/1/";
+// "https://interviewmania.com/gk-general-knowledge/art-and-culture/1/1#";
 
-axios
-  .get(url)
-  .then((response) => {
+async function scrapPage(pageNumber) {
+  let url = `${baseurl}${pageNumber}`;
+  try {
+    const response = await axios.get(url);
     const html = response.data;
     const $ = cheerio.load(html);
     const questions = [];
-    // console.log(html);
 
+    // extracting questions
     $("ol[start] li div").each((index, element) => {
-      const question = $(element).text();
-      questions.push({ id: index + 1, question, options: [] ,answers:[]})
-      
+      const question = $(element).text().trim();
+      questions.push({ id: index + 1, question, options: [], answers: [] });
     });
-    
-    $(".list-group-item").each((i, el) => {
-        const options = [];
-        $(el).find('.row').each((I,El) => {
-          const option = $(El).find('span').text().trim();
-          options.push(option);
-        });
-       
-        if (questions[i]) {
-          questions[i].options = options;
-        }
-      });
-      $(".collapse").each((i, el) => {
-        const answer = $(el).find("span").text();
-        if (questions[i]) {
-          questions[i].answers = answer;
-        }
-      });
-      console.log(questions)
 
-    const parser = new Parser();
-    const csv = parser.parse(questions);
-    fs.writeFileSync('data.csv', csv, 'utf-8');
-  })
-  .catch((error) => {
-    console.log(error);
-    
-  });
+    // extract options
+
+    $(".list-group-item .row").each((i, el) => {
+      const options = [];
+      $(el).each((I, El) => {
+        const option = $(El).find("span").text().trim();
+        options.push(option);
+      });
+
+      if (questions[i]) {
+        questions[i].options = options;
+      }
+    });
+
+    // extract answers
+
+    $(".collapse h5").each((i, el) => {
+      const answer = $(el).find("span").text();
+      if (questions[i]) {
+        questions[i].answers = answer;
+      }
+    });
+
+    return questions;
+  } catch (error) {
+    console.log("error :", error.message);
+  }
+}
+
+async function Pagination() {
+  const allpage = [];
+  let pageNumber = 1;
+  for (let page = 1; ; pageNumber++) {
+    console.log(`Scraping page ${pageNumber}`);
+    const questions = await scrapPage(pageNumber);
+    if (!questions || questions.length === 0) {
+      console.log(`No more page ${pageNumber}. Stop.`);
+      break;
+    }
+    allpage.push(...questions);
+  }
+  const parser = new Parser();
+  const csv = parser.parse(allpage);
+  fs.writeFileSync("data.csv", csv, "utf-8");
+}
+
+Pagination();
